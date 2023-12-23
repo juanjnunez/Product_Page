f28f7dfd1a96a152129cc4ddff01eeb268dec5e4
juanjnunez <juanjnunez@yahoo.com>
2023-12-22 20:21:55 -0500

    created components and functionality

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
  const app = Vue.createApp({
-    data() {
-        return{
-            product: 'Socks'
-        }
+   data(){
+    return {
+        cart: [],
+        premium: true
     }
+   },
+
+   methods: {
+    updateCart(id){
+        this.cart.push(id);
+    }
+   }
  })
